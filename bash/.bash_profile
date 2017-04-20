331f18bbdea0bc5fa3f5fb72571623f11a322b37
Christian Rondeau <christian.rondeau@gmail.com>
2017-04-20 07:40:57 -0400

    stow: bash

---
+++ bash/.bash_profile
@@ -0,0 +1,16 @@
+# Configure terminal with colors
+export TERM=xterm-256color
+
+# Configure often-used aliases
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias posh="powershell -File "
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gca="git commit --amend"
+alias gp="git push"
+alias gu="git pull"
+
--- setup.sh
+++ setup.sh
@@ -5,18 +5,27 @@ if [ "$EUID" -ne 0 ]; then
 	exit 1
 fi
 
+######################################### Prepare
+
 apt-get update
 apt-get upgrade -y
 
 cd $(dirname "$0")
 
-#TODO: bash
+######################################### Bash
+
+stow bash
+
+######################################### Git
 
 stow git
 if [ hash vim 2>/dev/null ]; then
 	apt-get install git -y
 fi
 #TODO: autocrlf = true on cygwin
+#TODO: different email on different machines (env variables?)
+
+######################################### Vim
 
 stow vim
 if [ hash vim 2>/dev/null ]; then
