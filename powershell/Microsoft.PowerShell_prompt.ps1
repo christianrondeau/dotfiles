d29d3616468dace21c510ca2a9409f66d6a2344c
Christian Rondeau <christian.rondeau@mediaclip.ca>
2017-04-27 11:56:59 -0400

    Line break before PowerShell prompt

--- powershell/Microsoft.PowerShell_prompt.ps1
+++ powershell/Microsoft.PowerShell_prompt.ps1
@@ -1,6 +1,7 @@
 ﻿function prompt {
 	$realLASTEXITCODE = $LASTEXITCODE
 	$host.UI.RawUI.WindowTitle = Microsoft.PowerShell.Management\Split-Path $pwd.ProviderPath -Leaf
+	Microsoft.PowerShell.Utility\Write-Host
 	Microsoft.PowerShell.Utility\Write-Host $env:COMPUTERNAME -NoNewLine -ForegroundColor "White"
 	Microsoft.PowerShell.Utility\Write-Host ":" -NoNewLine -ForegroundColor "DarkGray"
 	Microsoft.PowerShell.Utility\Write-Host $pwd.ProviderPath -NoNewLine -ForegroundColor "Green"
