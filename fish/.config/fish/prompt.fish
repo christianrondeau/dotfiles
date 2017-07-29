fccde53a3779e4e7050108b867852b6ddaa310c2
Christian Rondeau <christian.rondeau@gmail.com>
2017-07-29 16:14:15 -0400

    Basic but working fish prompt

--- fish/.config/fish/prompt.fish
+++ fish/.config/fish/prompt.fish
@@ -1,6 +1,6 @@
-#function fish_mode_prompt; end
+function fish_mode_prompt; end
 
-# function fish_prompt
+function fish_prompt
 # 	set -l last_status $status
 # 	set -l fish_prompt_dim 946638
 # 	
@@ -10,17 +10,17 @@
 # 		printf "[E$last_status] "
 # 	end
 # 
-# 	set_color normal
-# 	if [ $COMPUTERNAME ]
-# 		echo -n $COMPUTERNAME
-# 	else
-# 		echo -n (hostname)
-# 	end
-# 	set_color $fish_prompt_dim
-# 	echo -n ':'
-# 	set_color $fish_color_cwd
-# 	echo -n (prompt_pwd)
-# 
+	set_color normal
+	if [ $COMPUTERNAME ]
+		echo -n $COMPUTERNAME
+	else
+		echo -n (hostname)
+	end
+	set_color $fish_prompt_dim
+	echo -n ':'
+	set_color $fish_color_cwd
+	echo -n (prompt_pwd)
+
 # 	set -l git_branch (git rev-parse --abbrev-ref HEAD ^ /dev/null)
 # 	if [ $status -eq 0 ]
 # 		set -l git_dirty_count (git status --porcelain  | wc -l | sed "s/ //g")
@@ -33,29 +33,29 @@
 # 		end
 # 	end
 # 
-# 	printf "\n"
-# 
-# 	if set -q __fish_vi_mode
-# 		switch $fish_bind_mode
-# 		case default
-# 			set_color black --background green
-# 			printf "λ"
-# 		case insert
-# 			set_color $fish_prompt_dim
-# 			printf "λ"
-# 		case visual
-# 			set_color black --background blue
-# 			printf "λ"
-# 		case replace-one
-# 			set_color black --background red
-# 			printf "λ"
-# 		end
-# 		set_color normal
-# 	else
-# 		set_color $fish_prompt_dim
-# 		printf "λ"
-# 	end
-# 
-# 	set_color $fish_prompt_normal
-# 	printf " "
-# end
+	printf "\n"
+
+	if set -q __fish_vi_mode
+		switch $fish_bind_mode
+		case default
+			set_color black --background green
+			printf "λ"
+		case insert
+			set_color $fish_prompt_dim
+			printf "λ"
+		case visual
+			set_color black --background blue
+			printf "λ"
+		case replace-one
+			set_color black --background red
+			printf "λ"
+		end
+		set_color normal
+	else
+		set_color $fish_prompt_dim
+		printf "λ"
+	end
+
+	set_color $fish_prompt_normal
+	printf " "
+end
