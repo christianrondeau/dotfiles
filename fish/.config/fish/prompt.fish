cad6f5f5785532cabd8396b4488940cb0c556e85
Christian Rondeau <christian.rondeau@gmail.com>
2017-07-30 10:37:20 -0400

    Restore fish prompt: see fish#4269

--- fish/.config/fish/prompt.fish
+++ fish/.config/fish/prompt.fish
@@ -1,15 +1,15 @@
 function fish_mode_prompt; end
 
 function fish_prompt
-# 	set -l last_status $status
-# 	set -l fish_prompt_dim 946638
-# 	
-# 	printf "\n\033[K"
-# 	if test $last_status -ne 0
-# 		set_color $fish_color_error
-# 		printf "[E$last_status] "
-# 	end
-# 
+	set -l last_status $status
+	set -l fish_prompt_dim 946638
+	
+	printf "\n\033[K"
+	if test $last_status -ne 0
+		set_color $fish_color_error
+		printf "[E$last_status] "
+	end
+
 	set_color normal
 	if [ $COMPUTERNAME ]
 		echo -n $COMPUTERNAME
@@ -21,18 +21,18 @@ function fish_prompt
 	set_color $fish_color_cwd
 	echo -n (prompt_pwd)
 
-# 	set -l git_branch (git rev-parse --abbrev-ref HEAD ^ /dev/null)
-# 	if [ $status -eq 0 ]
-# 		set -l git_dirty_count (git status --porcelain  | wc -l | sed "s/ //g")
-# 		if [ $git_dirty_count -gt 0 ]
-# 			set_color $fish_color_error
-# 			echo -n ' ['$git_branch']'
-# 		else
-# 			set_color green
-# 			echo -n ' ['$git_branch']'
-# 		end
-# 	end
-# 
+	set -l git_branch (git rev-parse --abbrev-ref HEAD ^ /dev/null)
+	if [ $status -eq 0 ]
+		set -l git_dirty_count (git status --porcelain  | wc -l | sed "s/ //g")
+		if [ $git_dirty_count -gt 0 ]
+			set_color $fish_color_error
+			echo -n ' ['$git_branch']'
+		else
+			set_color green
+			echo -n ' ['$git_branch']'
+		end
+	end
+
 	printf "\n"
 
 	if set -q __fish_vi_mode
