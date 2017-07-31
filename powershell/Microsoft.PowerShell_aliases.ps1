52d9a7b870ab07cc81c7d1bc8f09e1ca56e1d83c
Christian Rondeau <christian.rondeau@mediaclip.ca>
2017-07-31 11:58:40 -0400

    ggroot to go to a git root in PowerShell

--- powershell/Microsoft.PowerShell_aliases.ps1
+++ powershell/Microsoft.PowerShell_aliases.ps1
@@ -1,33 +1,12 @@
-﻿function Git-Status { git status }
-Set-Alias -Name ggs -Value Git-Status
-
-function Git-Add { git add }
+﻿Set-Alias -Name ggs -Value Git-Status
 Set-Alias -Name gga -Value Git-Add
-
-function Git-AddAll  { git add -A }
 Set-Alias -Name ggaa -Value Git-AddAll
-
-function Git-Commit { git commit }
 Set-Alias -Name ggc -Value Git-Commit
-
-function Git-Amend  { git commit --amend }
 Set-Alias -Name ggca -Value Git-Amend
-
-function Git-Push { git push }
 Set-Alias -Name ggp -Value Git-Push
-
-function Git-Pull { git pull }
 Set-Alias -Name ggu -Value Git-Pull
-
-function Git-Diff { git diff }
 Set-Alias -Name ggd -Value Git-Diff
-
-function Git-Log  { git log --graph --color --all --decorate --format="%C(auto)%d %s" }
 Set-Alias -Name ggl -Value Git-Log
-
-function Git-LogVerbose  { git log --graph --color --all --decorate --format="%C(auto)%h %d %s %Cblue %ar %an" }
 Set-Alias -Name ggll -Value Git-LogVerbose
-
-function Git-Current  { git show -s --format='%Cgreen%h %Cblue%an %Cred%cr%Creset%n%s' }
 Set-Alias -Name ggx -Value Git-Current
-
+Set-Alias -Name ggroot -Value Git-Root
--- powershell/Microsoft.PowerShell_functions.ps1
+++ powershell/Microsoft.PowerShell_functions.ps1
@@ -1,4 +1,21 @@
+# Linux-like Shortcuts
+
 function which($name)
 {
     Get-Command $name | Select-Object -ExpandProperty Definition
 }
+
+# Git Shortcuts
+
+function Git-Status { git status }
+function Git-Add { git add }
+function Git-AddAll  { git add -A }
+function Git-Commit { git commit }
+function Git-Amend  { git commit --amend }
+function Git-Push { git push }
+function Git-Pull { git pull }
+function Git-Diff { git diff }
+function Git-Log  { git log --graph --color --all --decorate --format="%C(auto)%d %s" }
+function Git-LogVerbose  { git log --graph --color --all --decorate --format="%C(auto)%h %d %s %Cblue %ar %an" }
+function Git-Current  { git show -s --format='%Cgreen%h %Cblue%an %Cred%cr%Creset%n%s' }
+function Git-Root() { pushd (git rev-parse --show-toplevel) }
