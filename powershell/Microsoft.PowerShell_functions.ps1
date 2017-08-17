c22253509da9a3029b52d7b7f0a798cf50890d54
Christian Rondeau <christian.rondeau@mediaclip.ca>
2017-08-17 12:31:10 -0400

    Allow passing a commit message to ggc in Powershell

--- powershell/Microsoft.PowerShell_functions.ps1
+++ powershell/Microsoft.PowerShell_functions.ps1
@@ -7,17 +7,17 @@ function which($name)
 
 # Git Shortcuts
 
-function ggs{ git status }
+function ggs { git status }
 function gga { git add }
-function ggaa  { git add -A }
-function ggc { git commit }
-function ggca  { git commit --amend }
-function ggcaa  { git commit --amend --no-edit }
+function ggaa { git add -A }
+function ggc($msg) { if($msg) { git commit -m $msg } else { git commit } }
+function ggca { git commit --amend }
+function ggcaa { git commit --amend --no-edit }
 function ggp { git push }
 function ggu { git pull --ff-only }
 function ggd { git diff }
 function ggds { git diff --staged }
-function ggl  { git log --graph --color --all --decorate --format="%C(auto)%d %s" }
-function ggll  { git log --graph --color --all --decorate --format="%C(auto)%h %d %s %Cblue %ar %an" }
+function ggl { git log --graph --color --all --decorate --format="%C(auto)%d %s" }
+function ggll { git log --graph --color --all --decorate --format="%C(auto)%h %d %s %Cblue %ar %an" }
 function ggx { git show -s --format='%Cgreen%h %Cblue%an %Cred%cr%Creset%n%s' }
 function ggroot { pushd (git rev-parse --show-toplevel) }
