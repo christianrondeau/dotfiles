b316d8137f7ff38fcfbcebf970ebb7a1564cc37a
Christian Rondeau <christian.rondeau@gmail.com>
2017-09-03 09:32:05 -0400

    Load .env in bash with quotes

--- fish/.config/fish/functions/posix-source.fish
+++ fish/.config/fish/functions/posix-source.fish
@@ -1,6 +1,6 @@
 function posix-source
 	for i in (cat $argv)
-		set arr (echo $i | string match -r "([^=]+)=(.*)")
+		set arr (echo $i | string match -r "([^=]+)='?([^']+)")
 		set -gx $arr[2] $arr[3]
 	end
 end
