e8bcc32b6da35b5b510ad9c4fef1635924d4fc2e
Christian Rondeau <christian.rondeau@gmail.com>
2017-09-12 20:32:08 -0400

    PowerShell update script

---
+++ update.ps1
@@ -0,0 +1,12 @@
+﻿#Requires -RunAsAdministrator
+
+Set-StrictMode -version Latest
+
+$DotFilesPath = Split-Path $MyInvocation.MyCommand.Path
+pushd $DotFilesPath
+try {
+	./cup all -y
+	./gvim -c "PlugUpdate" -c "qa!"
+} finally {
+	popd
+}
